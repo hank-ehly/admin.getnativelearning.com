268c48be84061d62eab08fe64fb4bff2e5289571
Hank Ehly <henry.ehly@gmail.com>
2017-05-19 14:53:19 +0900

    Add some tests to VideoTranscriptionService

--- src/app/videos/video-transcription.service.spec.ts
+++ src/app/videos/video-transcription.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { VideoTranscriptionService } from './video-transcription.service';
+import { Observable } from 'rxjs/Observable';
 
 describe('VideoTranscriptionService', () => {
     beforeEach(() => {
@@ -11,5 +12,20 @@ describe('VideoTranscriptionService', () => {
 
     it('should be created', inject([VideoTranscriptionService], (service: VideoTranscriptionService) => {
         expect(service).toBeTruthy();
+    }));
+
+    it('should take a single argument of File type', inject([VideoTranscriptionService], (service: VideoTranscriptionService) => {
+        const file = new File([], '');
+        expect(() => service.transcribeVideoFile(file)).not.toThrow();
+    }));
+
+    it('should not return null', inject([VideoTranscriptionService], (service: VideoTranscriptionService) => {
+        const file = new File([], '');
+        expect(typeof service.transcribeVideoFile(file)).not.toBeNull();
+    }));
+
+    it('should return an Observable of type string', inject([VideoTranscriptionService], (service: VideoTranscriptionService) => {
+        const file = new File([], '');
+        expect(service.transcribeVideoFile(file).constructor.name).toEqual('Observable');
     }));
 });
--- src/app/videos/video-transcription.service.ts
+++ src/app/videos/video-transcription.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+
 @Injectable()
 export class VideoTranscriptionService {
-    constructor() {
+
+    transcribeVideoFile(file: File): Observable<string> {
+        return new Observable<string>();
     }
+
 }
