a63c9b1c2ebdf21ea30e11c7f382f26ea9db257e
Hank Ehly <henry.ehly@gmail.com>
2017-05-30 07:47:40 +0900

    Add specs to login component

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -24,4 +24,31 @@ describe('LoginComponent', () => {
     it('should be created', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should have a credentials model', () => {
+        expect(component.credentials).toBeTruthy();
+    });
+
+    it('should have a blank email credential upon page load', () => {
+        expect(component.credentials.email).toBe('');
+    });
+
+    it('should display an empty email input upon page load', () => {
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('input[type=email]').value).toEqual('');
+    });
+
+    it('should have a blank password credential upon page load', () => {
+        expect(component.credentials.email).toBe('');
+    });
+
+    it('should display an empty password input upon page load', () => {
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('input[type=password]').value).toEqual('');
+    });
+
+    it('should disable the login button by default', () => {
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('button[type=submit]').disabled).toEqual(true);
+    });
 });
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    credentials = {
+        email: '',
+        password: ''
+    };
+
     constructor() {
     }
 
