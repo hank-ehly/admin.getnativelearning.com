6a9f68c50e6fb50f2636d730fb280a4e063c05d1
Hank Ehly <henry.ehly@gmail.com>
2017-05-30 11:00:13 +0900

    Remove unused code

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,13 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
-import { By } from '@angular/platform-browser';
-
-function newEvent(eventName: string, bubbles = false, cancelable = false) {
-    const evt = document.createEvent('CustomEvent');  // MUST be 'CustomEvent'
-    evt.initCustomEvent(eventName, bubbles, cancelable, null);
-    return evt;
-}
 
 describe('LoginComponent', () => {
     let component: LoginComponent;
