614408376490fb9ffc91261f17269420979a00f5
Hank Ehly <henry.ehly@gmail.com>
2017-06-14 10:40:45 +0900

    Remove unused forbid directive

--- src/app/shared/forbid/forbid.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ForbidDirective } from './forbid.directive';
-
-describe('ForbidDirective', () => {
-    it('should create an instance', () => {
-        const directive = new ForbidDirective();
-        expect(directive).toBeTruthy();
-    });
-});
