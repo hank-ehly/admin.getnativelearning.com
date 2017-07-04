4fd46201b056cf6e59a72b1a264e4d11561d1d3c
Hank Ehly <henry.ehly@gmail.com>
2017-07-04 10:47:22 +0900

    Add speaker service

--- src/app/speaker/speaker.module.ts
+++ src/app/speaker/speaker.module.ts
@@ -4,13 +4,15 @@ import { IndexSpeakerComponent } from './index/index.component';
 import { ShowSpeakerComponent } from './show/show.component';
 import { SpeakerComponent } from './speaker.component';
 import { SpeakerFormComponent } from './form/form.component';
+import { SpeakerService } from './speaker.service';
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations: [IndexSpeakerComponent, ShowSpeakerComponent, SpeakerComponent, SpeakerFormComponent],
-    exports: [IndexSpeakerComponent, ShowSpeakerComponent, SpeakerComponent, SpeakerFormComponent]
+    exports: [IndexSpeakerComponent, ShowSpeakerComponent, SpeakerComponent, SpeakerFormComponent],
+    providers: [SpeakerService]
 })
 export class SpeakerModule {
 }
