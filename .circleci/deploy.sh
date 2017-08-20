6ccdac8265c06d9ebb6690516725163d2e1627fa
Hank Ehly <henry.ehly@gmail.com>
2017-08-20 12:48:23 +0900

    Add logging to deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,9 +6,11 @@ if [ -z ${1} ]; then
 fi
 
 if [ "${1}" == "develop" ]; then
+	echo "** Deploying to staging environment **"
 	git clone git@github.com:hank-ehly/devops.getnativelearning.com.git
 	cd devops.getnativelearning.com/deploy && bundle install --jobs 4 --path vendor/bundle && bundle exec cap admin:staging deploy
 elif [ "${1}" == "master" ]; then
+	echo "** Deploying to production environment **"
 	git clone git@github.com:hank-ehly/devops.getnativelearning.com.git
 	cd devops.getnativelearning.com/deploy && bundle install --jobs 4 --path vendor/bundle && bundle exec cap admin:production deploy
 fi
